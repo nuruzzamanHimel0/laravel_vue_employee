9c0b5078fc9040d420669adb36251b556afbbb1e
nuruzzamanhimel0 <nuruzzaman.himel147@gmail.com>
2021-11-17 17:10:50 +0600

    migration is done

--- database/migrations/2021_11_17_055927_create_depertments_table.php
+++ database/migrations/2021_11_17_055927_create_depertments_table.php
@@ -15,6 +15,7 @@ class CreateDepertmentsTable extends Migration
     {
         Schema::create('depertments', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
             $table->timestamps();
         });
     }
